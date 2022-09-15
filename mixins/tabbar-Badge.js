1fa9542f3326032b4d00d56177cda648caf49424
AOBBPLUS <97869330+AOBBPLUS@users.noreply.github.com>
2022-09-15 11:46:52 +0800

    登录和支付功能

--- mixins/tabbar-Badge.js
+++ mixins/tabbar-Badge.js
@@ -14,6 +14,7 @@ export default {
       // 调用 uni.setTabBarBadge() 方法，为购物车设置右上角的徽标
       if (this.total !== 0){
         uni.setTabBarBadge({
+        
         index: 2,
         text: this.total + '', // 注意：text 的值必须是字符串，不能是数字
       })
